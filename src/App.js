a4d0fc06ee17294d5c50ecdb1ace4bcd8eeafb42
unknown <nitesh44bhatta@gmail.com>
2023-05-24 20:00:42 +0545

    vercel

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
-// import Navbar from "./components/Navbar";
-// import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
-// import Database from "./pages/Database";
+import Database from "./pages/Database";
 
 function App() {
   return (
     <>
     <Home />
-      {/* <BrowserRouter> */}
-        {/* <Routes> */}
-          {/* <Route path= "/" element={<Home /> }/> */}
-          {/* <Route path= "/database" element={<Database /> }/> */}
-        {/* </Routes> */}
-      {/* </BrowserRouter> */}
+      <BrowserRouter>
+        <Routes>
+          <Route path= "/" element={<Home /> }/>
+          <Route path= "/database" element={<Database /> }/>
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
