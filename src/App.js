16740651c287d3d885f686df54c3a35d50f52cb7
unknown <nitesh44bhatta@gmail.com>
2023-05-24 19:50:56 +0545

    app

--- src/App.js
+++ src/App.js
@@ -1,17 +1,18 @@
 // import Navbar from "./components/Navbar";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+// import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
-import Database from "./pages/Database";
+// import Database from "./pages/Database";
 
 function App() {
   return (
     <>
-      <BrowserRouter>
-        <Routes>
-          <Route path= "/" element={<Home /> }/>
-          <Route path= "/database" element={<Database /> }/>
-        </Routes>
-      </BrowserRouter>
+    <Home />
+      {/* <BrowserRouter> */}
+        {/* <Routes> */}
+          {/* <Route path= "/" element={<Home /> }/> */}
+          {/* <Route path= "/database" element={<Database /> }/> */}
+        {/* </Routes> */}
+      {/* </BrowserRouter> */}
     </>
   );
 }
