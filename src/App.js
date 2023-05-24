20e6c37881acdc38ca0c884f4efdf1f761f0fdf7
unknown <nitesh44bhatta@gmail.com>
2023-05-24 20:07:43 +0545

    vercel2

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import Navbar from "./components/Navbar";
+// import Navbar from "./components/Navbar";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Database from "./pages/Database";
